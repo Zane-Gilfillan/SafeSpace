3a40c72fb8fc3537093c40ea27b7f9b7875260b9
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-03-22 15:33:10 -0500

    updated array

--- javascript/map-data.js
+++ javascript/map-data.js
@@ -129,20 +129,20 @@ const locations =
   {
     "lat": 41.8888675,
     "lng": -87.6243635,
-    "name": "McCormick bridgehouse and Chicago River museum",
-    "streetName": "Michigan Avenue bridge/99 Chicago Riverwalk "
+    "name": "McCormick Bridgehouse and Chicago River Museum",
+    "streetName": "Michigan Avenue Bridge/99 Chicago Riverwalk "
   },
   {
     "lat": 41.8526105,
     "lng": -87.6228241,
     "name": "Hampton Inn Chicago McCormick Place",
-    "streetName": "123 E Cermak Rd Suite 100, Chicago, IL 60616"
+    "streetName": "123 E. Cermak Road Suite 100"
   },
   {
     "lat": 41.790101,
     "lng": -87.601238,
     "name": "Cobb Hall - University of Chicago",
-    "streetName": "5811 S Ellis Avenue"
+    "streetName": "5811 S. Ellis Avenue"
   },
   {
     "lat": 41.7914,
@@ -160,7 +160,7 @@ const locations =
     "lat": 41.88027,
     "lng": -87.62445,
     "name": "The School of the Art Institute of Chicago",
-    "streetName": "112 S Michigan Ave"
+    "streetName": "112 S. Michigan Ave"
   },
   {
     "lat": 41.8809849,
@@ -178,7 +178,7 @@ const locations =
     "lat": 41.8711357,
     "lng": -87.6716935,
     "name": "Project WISH at the University of Illinois at Chicago",
-    "streetName": "840 South Wood Street Room B39 (basement)"
+    "streetName": "840 S. Wood Street Room B39 (Basement)"
   },
   {
     "lat": 42.0015036,
@@ -208,13 +208,13 @@ const locations =
     "lat": 41.8977465,
     "lng": -87.6270445,
     "name": "Loyola University Chicago",
-    "streetName": "26 E. Pearson St."
+    "streetName": "26 E. Pearson St"
   },
   {
     "lat": 41.7860645,
     "lng": -87.5913825,
     "name": "Chicago Theological Seminary",
-    "streetName": "1407 E. 60th St. "
+    "streetName": "1407 E. 60th St "
   },
   {
     "lat": 41.7956199,
@@ -226,73 +226,73 @@ const locations =
     "lat": 41.8737874,
     "lng": -87.6246995,
     "name": "Columbia College Chicago",
-    "streetName": "618 South Michigan Avenue"
+    "streetName": "618 S. Michigan Avenue"
   },
   {
     "lat": 41.8737874,
     "lng": -87.6246995,
     "name": "Columbia College Chicago",
-    "streetName": "618 South Michigan Avenue"
+    "streetName": "618 S. Michigan Avenue"
   },
   {
     "lat": 41.8737874,
     "lng": -87.6246995,
     "name": "Columbia College Chicago",
-    "streetName": "618 S Michigan "
+    "streetName": "618 S. Michigan"
   },
   {
     "lat": 41.7661381,
     "lng": -87.568141,
     "name": "Chicago Women's Project",
-    "streetName": "2317 E 71st "
+    "streetName": "2317 E. 71st St"
   },
   {
     "lat": 41.9779606,
     "lng": -87.6602131,
-    "name": "Chicago grind coffeehouse ",
-    "streetName": "5256 n broadway "
+    "name": "Chicago Grind Coffeehouse ",
+    "streetName": "5256 N. Broadway"
   },
   {
     "lat": 41.892353,
     "lng": -87.6273233,
     "name": "Freehand Chicago Hotel/Hostel",
-    "streetName": "19 E Ohio St "
+    "streetName": "19 E. Ohio St"
   },
   {
     "lat": 41.9097355,
     "lng": -87.6768698,
     "name": "Bru Chicago",
-    "streetName": "1562 N Milwaukee"
+    "streetName": "1562 N. Milwaukee"
   },
   {
     "lat": 41.9183203,
     "lng": -87.653169,
-    "name": "952-954 W Armitage Ave, Chicago, IL 60614, USA",
-    "streetName": "952-954 W Armitage Ave"
+    "name": "Armitage CTA Station",
+    "streetName": "952-954 W. Armitage Ave"
   },
   {
     "lat": 41.882564,
     "lng": -87.6374246,
     "name": "Lyric Opera of Chicago",
-    "streetName": "20 N Wacker Dr"
+    "streetName": "20 N. Wacker Dr"
   },
   {
     "lat": 41.7879593,
     "lng": -87.5995794,
     "name": "University of Chicago",
-    "streetName": "1116 E 59th St, "
+    "streetName": "1116 E. 59th St, "
   },
   {
     "lat": 41.881662,
     "lng": -87.6249685,
     "name": "Chicago Athletic Association",
-    "streetName": "12 S Michigan Ave"
+    "streetName": "12 S. Michigan Ave"
   },
   {
     "lat": 41.881662,
     "lng": -87.6249685,
     "name": "Chicago Athletic Association",
-    "streetName": "12 S Michigan Ave"
+    "streetName": "12 S. Michigan Ave"
   },
   {
     "lat": 41.9296617,
@@ -304,31 +304,31 @@ const locations =
     "lat": 41.8809923,
     "lng": -87.6256487,
     "name": "School of The Art Institute of Chicago - Sharp Building",
-    "streetName": "37 S Wabash"
+    "streetName": "37 S. Wabash"
   },
   {
     "lat": 41.8881532,
     "lng": -87.6337102,
     "name": "The Chicago School of Professional Psychology",
-    "streetName": "325 N Wells St"
+    "streetName": "325 N. Wells St"
   },
   {
     "lat": 41.8718079,
     "lng": -87.6709492,
     "name": "Library of the Health Sciences, University of Illinois at Chicago",
-    "streetName": "1750 W. Polk St."
+    "streetName": "1750 W. Polk St"
   },
   {
     "lat": 41.8921448,
     "lng": -87.6074731,
     "name": "Chicago Shakespeare Theater",
-    "streetName": "800 E Grand Ave"
+    "streetName": "800 E. Grand Ave"
   },
   {
     "lat": 41.8916405,
     "lng": -87.6061554,
     "name": "The Yard at Chicago Shakespeare Theater",
-    "streetName": "800 E Grand Ave"
+    "streetName": "800 E. Grand Ave"
   },
   {
     "lat": 41.8918613,
@@ -339,26 +339,26 @@ const locations =
   {
     "lat": 41.8038062,
     "lng": -87.5818865,
-    "name": "S Lake Shore Dr, Chicago, IL 60637, USA",
-    "streetName": "S Lake Shore Dr"
+    "name": "S. Lake Shore Dr, Chicago, IL 60637, USA",
+    "streetName": "S. Lake Shore Dr"
   },
   {
     "lat": 41.8038062,
     "lng": -87.5818865,
-    "name": "S Lake Shore Dr, Chicago, IL 60637, USA",
-    "streetName": "S Lake Shore Dr"
+    "name": "S. Lake Shore Dr, Chicago, IL 60637, USA",
+    "streetName": "S. Lake Shore Dr"
   },
   {
     "lat": 41.9118171,
     "lng": -87.6316716,
     "name": "Chicago History Museum",
-    "streetName": "1601 N Clark St"
+    "streetName": "1601 N. Clark St"
   },
   {
     "lat": 41.9767901,
     "lng": -87.9008276,
-    "name": "Terminal 3, Chicago, IL 60666, USA",
-    "streetName": "Terminal 3"
+    "name": "Terminal 3 ORD",
+    "streetName": "10000 W O'Hare Ave Terminal 3"
   },
   {
     "lat": 41.9801774,
@@ -370,19 +370,7 @@ const locations =
     "lat": 41.8795473,
     "lng": -87.6237238,
     "name": "Chicago Institute of Art",
-    "streetName": "111 S Michigan Ave"
-  },
-  {
-    "lat": 37.8462952,
-    "lng": -122.252171,
-    "name": "Zachary's Chicago Pizza",
-    "streetName": "5801 College Ave"
-  },
-  {
-    "lat": 37.8916625,
-    "lng": -122.278576,
-    "name": "Zachary Chicago Pizza",
-    "streetName": "1853 Solano Ave"
+    "streetName": "111 S. Michigan Ave"
   },
   {
     "lat": 42.0935807,
@@ -391,76 +379,70 @@ const locations =
     "streetName": "2100 Patriot Boulevard"
   },
   {
-    "lat": 37.8462952,
-    "lng": -122.252171,
-    "name": "Zachary's Chicago pizza",
-    "streetName": "5801 College Ave"
-  },
-  {
     "lat": 41.8934776,
     "lng": -87.6261792,
     "name": "Trader Joe's",
-    "streetName": "44 E Ontario St, Chicago, IL 60611"
+    "streetName": "44 E. Ontario St"
   },
   {
     "lat": 41.8955134,
     "lng": -87.6901831,
     "name": "Star Lounge Coffee Bar",
-    "streetName": "2521 W. Chicago Ave. "
+    "streetName": "2521 W. Chicago Ave"
   },
   {
     "lat": 41.8821231,
     "lng": -87.6254547,
     "name": "Ryo Sushi",
-    "streetName": "62 E Madison St, Chicago, IL 60602"
+    "streetName": "62 E. Madison St"
   },
   {
     "lat": 41.8964025,
     "lng": -87.6641516,
     "name": "Beauty Bar",
-    "streetName": "1444 W. Chicago Ave. "
+    "streetName": "1444 W. Chicago Ave"
   },
   {
     "lat": 41.8963023,
     "lng": -87.6679454,
     "name": "The Shapiro Ballroom/I Do Dance Studio",
-    "streetName": "1612 W Chicago Ave"
+    "streetName": "1612 W. Chicago Ave"
   },
   {
     "lat": 41.9299792,
     "lng": -87.6438133,
     "name": "Snow Gragon Shavery",
-    "streetName": "2618 N Clark St, Chicago, IL 60614"
+    "streetName": "2618 N. Clark St"
   },
   {
     "lat": 41.9115838,
     "lng": -87.6350407,
-    "name": "Second City ",
-    "streetName": "1616 N Wells St, Chicago, IL 60614"
+    "name": "Second City",
+    "streetName": "1616 N. Wells St"
   },
   {
     "lat": 41.8962514,
     "lng": -87.6219521,
-    "name": "Lurie Children's hospital",
-    "streetName": "225 E. Chicago ave "
+    "name": "Lurie Children's Hospital",
+    "streetName": "225 E. Chicago Ave"
   },
   {
     "lat": 41.8971824,
     "lng": -87.621029,
     "name": "Museum of Contemporary Art",
-    "streetName": "220 E Chicago Ave"
+    "streetName": "220 E. Chicago Ave"
   },
   {
     "lat": 41.895943,
     "lng": -87.6944109,
-    "name": "Heritage restaurant ",
-    "streetName": "2700 w chicago ave"
+    "name": "Heritage Restaurant",
+    "streetName": "2700 W. Chicago Ave"
   },
   {
     "lat": 41.8957142,
     "lng": -87.676773,
     "name": "Atomic Cafe",
-    "streetName": "1957 W Chicago Ave"
+    "streetName": "1957 W. Chicago Ave"
   },
   {
     "lat": 44.9445763,
@@ -484,19 +466,19 @@ const locations =
     "lat": 42.0317989,
     "lng": -87.6786523,
     "name": "Hoosier Mama Pie Company and Dollop Coffee",
-    "streetName": "749 Chicago Ave"
+    "streetName": "749 W. Chicago Ave"
   },
   {
     "lat": 42.0317989,
     "lng": -87.6786523,
     "name": "Dollop Coffee and Hoosier Mama Pie Company",
-    "streetName": "749 Chicago ave"
+    "streetName": "749 W. Chicago Ave"
   },
   {
     "lat": 42.0317989,
     "lng": -87.6786523,
     "name": "Dollop Coffee and Hoosier Mama Pie Company",
-    "streetName": "749 Chicago Ave"
+    "streetName": "749 W. Chicago Ave"
   },
   {
     "lat": 42.0398215,
@@ -511,18 +493,6 @@ const locations =
     "streetName": "902 Chicago Ave"
   },
   {
-    "lat": 44.944621,
-    "lng": -93.2628817,
-    "name": "Modern Times Cafe",
-    "streetName": "3200 Chicago Ave S"
-  },
-  {
-    "lat": 44.944621,
-    "lng": -93.2628817,
-    "name": "Modern Times Cafe",
-    "streetName": "3200 Chicago Ave S"
-  },
-  {
     "lat": 44.9396943,
     "lng": -93.2629006,
     "name": "StevenBe",
@@ -532,13 +502,13 @@ const locations =
     "lat": 42.0475377,
     "lng": -87.6795313,
     "name": "Whole Foods Market",
-    "streetName": "1640 Chicago Ave."
+    "streetName": "1640 Chicago Ave"
   },
   {
     "lat": 41.7713482,
     "lng": -88.145646,
     "name": "Ward Residence Hall",
-    "streetName": "180 E Chicago Ave"
+    "streetName": "180 E. Chicago Ave"
   },
   {
     "lat": 42.8625366,
@@ -550,91 +520,91 @@ const locations =
     "lat": 41.994363,
     "lng": -87.657132,
     "name": "Metropolis",
-    "streetName": "1039 w granville"
+    "streetName": "1039 W. Granville"
   },
   {
     "lat": 41.976398,
     "lng": -87.668214,
     "name": "Jin Ju",
-    "streetName": "5203 N. Clark Street"
+    "streetName": "5203 N. Clark St"
   },
   {
     "lat": 41.921463,
     "lng": -87.664798,
-    "name": "Barnes & Noble Clybourn",
-    "streetName": "1441 W. WEBSTER AVE"
+    "name": "Barnes & Noble - Clybourn",
+    "streetName": "1441 W. Webster Ave"
   },
   {
     "lat": 41.955184,
     "lng": -87.65455,
     "name": "Howard Brown",
-    "streetName": "4025 N Sheridan Rd"
+    "streetName": "4025 N. Sheridan Rd"
   },
   {
     "lat": 41.983811,
     "lng": -87.656762,
     "name": "Zanzibar",
-    "streetName": "1036 N. Bryn Mawr Ave."
+    "streetName": "1036 N. Bryn Mawr Ave"
   },
   {
     "lat": 41.7883918,
     "lng": -87.598107,
-    "name": "Green Hall, UChicago campus",
-    "streetName": "5848 S. University Ave."
+    "name": "Green Hall, UChicago Campus",
+    "streetName": "5848 S. University Ave"
   },
   {
     "lat": 41.7910098,
     "lng": -87.5964657,
-    "name": "5710 LGBTQ and multicultural student center",
-    "streetName": "5710 S Woodlawn"
+    "name": "5710 LGBTQ and Multicultural Student Center",
+    "streetName": "5710 S. Woodlawn"
   },
   {
     "lat": 41.9069015,
     "lng": -87.6718357,
     "name": "Filter Coffee Shop",
-    "streetName": "1373-75 N Milwaukee Ave"
+    "streetName": "1373-75 N. Milwaukee Ave"
   },
   {
     "lat": 41.912881,
     "lng": -87.677341,
     "name": "Caffe de Luca",
-    "streetName": "1721 N Damen Ave"
+    "streetName": "1721 N. Damen Ave"
   },
   {
     "lat": 41.978284,
     "lng": -87.668518,
     "name": "George's Ice Cream and Sweets",
-    "streetName": "5306 N. Clark Street"
+    "streetName": "5306 N. Clark St"
   },
   {
     "lat": 42.007867,
     "lng": -87.668074,
     "name": "The Common Cup",
-    "streetName": "1501 W Morse Ave"
+    "streetName": "1501 W. Morse Ave"
   },
   {
     "lat": 41.9615203,
     "lng": -87.6750669,
     "name": "Glenn's Diner",
-    "streetName": "1820 W. Montrose Ave."
+    "streetName": "1820 W. Montrose Ave"
   },
   {
     "lat": 41.9233288,
     "lng": -87.6978093,
     "name": "Cafe Moustache",
-    "streetName": "2313 North Milwaukee Avenue"
+    "streetName": "2313 N. Milwaukee Avenue"
   },
   {
     "lat": 41.9422497,
     "lng": -87.6444881,
     "name": "Caribou Coffee",
-    "streetName": "3300 n broadway st"
+    "streetName": "3300 N. Broadway St"
   },
   {
     "lat": 41.8821873,
     "lng": -87.6256606,
-    "name": "Starbucks 68 E Madison",
-    "streetName": "68 E madison"
+    "name": "Starbucks",
+    "streetName": "68 E. Madison"
   },
   {
     "lat": 41.978595,
@@ -645,32 +615,32 @@ const locations =
   {
     "lat": 41.8934776,
     "lng": -87.6261792,
-    "name": "trader joe's",
-    "streetName": "44 East Ontario Street, "
+    "name": "Trader Joe's",
+    "streetName": "44 E. Ontario St"
   },
   {
     "lat": 41.9083507,
     "lng": -87.6525453,
     "name": "Whole Foods Market (Lincoln Park)",
-    "streetName": "1550 N. Kingsbury St."
+    "streetName": "1550 N. Kingsbury St"
   },
   {
     "lat": 42.000149,
     "lng": -87.660402,
     "name": "Loyola Univerity - Damen Student Center",
-    "streetName": "6461 North Sheridan Road"
+    "streetName": "6461 N. Sheridan Rd"
   },
   {
     "lat": 41.8985718,
     "lng": -87.6249154,
-    "name": "Fourth Presbyterian church",
-    "streetName": "126 E. Chestnut Avenue"
+    "name": "Fourth Presbyterian Church",
+    "streetName": "126 E. Chestnut Ave"
   },
   {
     "lat": 41.8985718,
     "lng": -87.6249154,
-    "name": "Fourth Presbyterian church",
-    "streetName": "126 E. Chestnut St."
+    "name": "Fourth Presbyterian Church",
+    "streetName": "126 E. Chestnut St"
   },
   {
     "lat": 41.8847013,
@@ -682,48 +652,48 @@ const locations =
     "lat": 41.8923096,
     "lng": -87.6269057,
     "name": "Pizzeria Uno",
-    "streetName": "29 E Ohio St "
+    "streetName": "29 E. Ohio St"
   },
   {
     "lat": 41.9251161,
     "lng": -87.6526156,
     "name": "DePaul University Art Museum",
-    "streetName": "935 W Fullerton Ave"
+    "streetName": "935 W. Fullerton Ave"
   },
   {
     "lat": 41.8804857,
     "lng": -87.6222864,
     "name": "Art Institute Modern Wing",
-    "streetName": "159 East Monroe"
+    "streetName": "159 E. Monroe"
   },
   {
     "lat": 41.9925101,
     "lng": -87.6603793,
-    "name": "lickety split",
-    "streetName": "6056 n broadway "
+    "name": "Lickety Split",
+    "streetName": "6056 N. Broadway"
   },
   {
     "lat": 41.942628,
     "lng": -87.702699,
-    "name": "Honey butter fried chicken",
+    "name": "Honey Butter Fried Chicken",
     "streetName": "3361 N. Elston Ave"
   },
   {
     "lat": 41.8817574,
     "lng": -87.6318285,
     "name": "Reconciling Ministries Network",
-    "streetName": "123 W Madison Street, 21st Floor"
+    "streetName": "123 W. Madison St, 21st Floor"
   },
   {
     "lat": 41.9979741,
     "lng": -87.6598257,
-    "name": "the coffee shop",
-    "streetName": "1135 w Sheridan rd"
+    "name": "The Coffee Shop",
+    "streetName": "1135 W. Sheridan Rd"
   },
   {
     "lat": 41.9886773,
     "lng": -87.660405,
-    "name": "North Broadway dental ",
-    "streetName": "5856 North Broadway"
+    "name": "North Broadway Dental",
+    "streetName": "5856 N. Broadway"
   }
 ]
